c2753072f1e731fbb2598470a07718cdfcbe5add
JRauer <rauer@technikum-wien.at>
2021-02-17 16:16:48 +0100

    Added morobot-2d

---
+++ examples/driveAroundXYZ_morobot-2d/driveAroundXYZ_morobot-2d.ino
@@ -0,0 +1,91 @@
+/**
+ *  \file driveAroundXYZ.ino
+ *  \brief Control the robot with the dabble app and drive around giving x-y-z-coordinates
+ *  @author	Johannes Rauer FHTW
+ *  @date	2020/11/27
+ *  
+ *  Hardware: 		- Arduino Mega (or similar microcontroller)
+ *  				- HC-05 bluetooth controller (or similar)
+ *  				- Calibrated morobot RRP
+ *  				- Powersupply 12V 5A (or more)
+ *  Connections:	- Powersupply to Arduino hollow connector
+ *  				- First smart servo of robot to Arduino:
+ *  					- Red cable to Vin
+ *  					- Black cable to GND
+ *  					- Yellow cable to pin 16 (TX2)
+ *  					- White calbe to pin 17 (RX2)
+ *  				- HC-05 to Arduino (Configuration to work with Dabble):
+ *  					- 5V to 5V
+ *  					- GND to GND
+ *  					- RX to pin 14 (TX3)
+ *  					- TX to pin 15 (RX3)
+ *  Install the Dabble-App on your smartphone or tablet
+ */
+
+#include <morobot_2d.h>
+#ifndef ESP32
+#include <Dabble.h>			// Include Dabble library for AVR-based controllers (Arduino) if no ESP32 is used
+#define DABBLE_PARAM 9600	// Set transmission speed
+#else
+#include <DabbleESP32.h>	// Include Dabble library for ESP32 board
+#define DABBLE_PARAM "MyEsp32" // Set bluetooth name
+#endif
+
+// Create morobot object and declare variables
+morobot_2d morobot;
+float step = 1.0;
+int delayDebounce = 250;
+float actPos[3];
+float actPosTemp[3];
+
+void setup() {
+	Dabble.begin(DABBLE_PARAM);		// Start connection to Dabble
+	morobot.begin("Serial2");		// The robot is connected to RX/TX2 -> Serial2
+	morobot.setSpeedRPM(50);
+	morobot.moveHome();				// Move the robot into initial position
+	delay(200);
+	initVars();
+	
+	Serial.println("Waiting for Dabble to connect to smartphone. If you are already connected, press any app-key.");
+	Dabble.waitForAppConnection();
+	Serial.println("Dabble connected!");
+	Serial.println("Use the gamepad to drive the robot around. Triangle and X buttons control vertical axis.");
+}
+
+void loop() {
+	// Get the input from the app
+	Dabble.processInput();
+	
+	if(GamePad.isPressed(2)) {			// Left
+		actPosTemp[0] = actPos[0] - step;
+	} else if(GamePad.isPressed(3)) {	// Right
+		actPosTemp[0] = actPos[0] + step;
+	} else if(GamePad.isPressed(0)) {	// Up
+		actPosTemp[2] = actPos[2] - step;
+	} else if(GamePad.isPressed(1)) {	// Down
+		actPosTemp[2] = actPos[2] + step;
+	} else if(GamePad.isPressed(7)) {	// O
+		morobot.moveHome();
+		initVars();
+	} else if(GamePad.isPressed(9)) {   // Square
+		morobot.getActPosition('x');
+		initVars();
+	}
+	
+	// If a movement comment has been given, move the robot
+	if (actPosTemp[0] != actPos[0] || actPosTemp[1] != actPos[1] || actPosTemp[2] != actPos[2]){
+		if (morobot.moveToPosition(actPosTemp[0], actPosTemp[1], actPosTemp[2]) == false){
+			delay(delayDebounce);
+			for (uint8_t i=0; i<3; i++) actPosTemp[i] = actPos[i];
+		} else {
+			for (uint8_t i=0; i<3; i++) actPos[i] = actPosTemp[i];
+		}
+	}
+}
+
+void initVars(){
+	actPos[0] = morobot.getActPosition('x');
+	actPos[1] = morobot.getActPosition('y');
+	actPos[2] = morobot.getActPosition('z');
+	for (uint8_t i=0; i<3; i++) actPosTemp[i] = actPos[i];
+}
---
+++ morobot_2d.cpp
@@ -0,0 +1,118 @@
+/**
+ *  \class 	morobot_2d
+ *  \brief 	morobot child class for ADD ROBOT TYPE for microcontrollers such as Arduino or ESP32
+ *  @file 	morobot_2d.h
+ *  @author	Johannes Rauer FHTW
+ *  @date	2021/02/17
+ *  \par Method List:
+ *  	public:
+ *  		newRobotClass() : morobotClass(PUT_NUM_SERVOS_HERE){};
+			virtual void setTCPoffset(float xOffset, float yOffset, float zOffset);
+			virtual bool checkIfAngleValid(uint8_t servoId, float angle);
+		protected:
+			virtual bool calculateAngles(float x, float y, float z);
+			virtual void updateTCPpose(bool output);
+ */
+ 
+#include <morobot_2d.h>
+
+void morobot_2d::setTCPoffset(float xOffset, float yOffset, float zOffset){
+	// Add given tcp-offset and default offsets
+	_tcpOffset[0] = xOffset + x_def_offset;
+	_tcpOffset[1] = yOffset + y_def_offset;
+	_tcpOffset[2] = zOffset + z_def_offset_bottom + z_def_offset_top;
+}
+
+bool morobot_2d::checkIfAngleValid(uint8_t servoId, float angle){
+	// The values are NAN if the inverse kinematics does not provide a solution
+	if(isnan(angle)){
+		Serial.print("Angle for motor ");
+		Serial.print(servoId);
+		Serial.println(" is NAN!");
+		return false;
+	}
+	
+	// Moving the motors out of the joint limits may harm the robot's mechanics
+	if(angle < _jointLimits[servoId][0] || angle > _jointLimits[servoId][1]) {
+		Serial.print("Angle for motor ");
+		Serial.print(servoId);
+		Serial.print(" is invalid! (");
+		Serial.print(angle);
+		Serial.println(" degrees).");
+		return false;
+	}
+	return true;
+}
+
+
+/* PROTECTED FUNCTIONS */
+bool morobot_2d::calculateAngles(float x, float y, float z){
+	// Subtract offset
+	x = x - _tcpOffset[0];
+	y = y - _tcpOffset[1];
+	z = z - _tcpOffset[2];
+	
+	// Calculate phi1
+	float L_M1_sq = pow(z,2) + pow(x,2);
+	float gamma = M_PI/2 - atan2(z,x);
+	float alpha = acos((L_M1_sq + pow(L1,2) - pow(L2,2)) / (2 * sqrt(L_M1_sq) * L1));
+	float phi1 = alpha - gamma;
+
+	// Calculate phi2
+	float L_M2_sq = pow(z,2) + pow(L4-L3-x,2);
+	float delta = M_PI/2 - atan2(z, L4-L3-x);
+	float beta = acos((L_M2_sq + pow(L1,2) - pow(L2,2)) / (2 * sqrt(L_M2_sq) * L1));
+	float phi2 = beta - delta;
+	
+	// Recalculate for motor mounting orientations in robot
+	phi1 = -phi1 * 180/M_PI + 90;
+	phi2 = phi2 * 180/M_PI - 90;
+	
+	// Check if angles are valid
+	if(checkIfAngleValid(0, phi1) == false) return false;
+	if(checkIfAngleValid(1, phi2) == false) return false;
+	
+	_goalAngles[0] = phi1;
+	_goalAngles[1] = phi2;
+	
+	return true;
+}
+
+void morobot_2d::updateTCPpose(bool output){
+	setBusy();
+	waitUntilIsReady();
+	
+	// Recalculate angles because of motor mounting orientations
+	float phi1 = - (getActAngle(0) - 90);
+	float phi2 = getActAngle(1) + 90;
+	phi1 = phi1 * M_PI/180;
+	phi2 = phi2 * M_PI/180;
+	
+	// Calculate positions of rotation axes
+	float Ax = -L1 * sin(phi1);
+	float Az = L1 * cos(phi1);
+	float Bx = L1 * sin(phi2) + L4 - L3;
+	float Bz = L1 * cos(phi2);
+	
+	// Calculate helping angles
+	float L_AB_sq = pow(Bz - Az, 2) + pow(Bx - Ax, 2);
+	float alpha = atan2(Bz-Az, Bx-Ax);
+	float beta = acos (sqrt(L_AB_sq) / (2 * L2));
+	float gamma = alpha + beta;
+	
+	// Calculate and store position
+	_actPos[0] = Ax + L2 * cos(gamma) + L3/2 + _tcpOffset[0];
+	_actPos[1] = _tcpOffset[1];
+	_actPos[2] = Az + L2 * sin(gamma) + _tcpOffset[2];
+	
+	if (output == true){
+		Serial.print("Calculated Position: ");
+		Serial.print(_actPos[0]);
+		Serial.print(", ");
+		Serial.print(_actPos[1]);
+		Serial.print(", ");
+		Serial.print(_actPos[2]);
+		Serial.print("; Orientation around z-axis [degrees]: ");
+		Serial.println(_actOri[2]);
+	}
+}

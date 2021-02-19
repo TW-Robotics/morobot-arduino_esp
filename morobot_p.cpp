9103e96d25071a3604c47aac49f5f4911fd8bc16
JRauer <rauer@technikum-wien.at>
2021-02-19 17:08:11 +0100

    Added morobot-p

---
+++ morobot_p.cpp
@@ -0,0 +1,147 @@
+/**
+ *  \class 	morobot_p
+ *  \brief 	morobot child class for morobot-p for microcontrollers such as Arduino or ESP32
+ *  @file 	morobot_p.h
+ *  @author	Johannes Rauer FHTW
+ *  @date	2020/11/27
+ *  \par Method List:
+ *  	public:
+ *  		morobot_p() : morobotClass(3){};
+			virtual void setTCPoffset(float xOffset, float yOffset, float zOffset);
+			virtual bool checkIfAngleValid(uint8_t servoId, float angle);
+			void moveHome();
+		protected:
+			virtual bool calculateAngles(float x, float y, float z);
+			virtual void updateCurrentXYZ();
+ */
+ 
+#include <morobot_p.h>
+
+void morobot_p::setTCPoffset(float xOffset, float yOffset, float zOffset){
+	_tcpOffset[0] = x_def_offset;
+	a3 = a3 + xOffset;
+	_tcpOffset[1] = yOffset;
+	_tcpOffset[2] = zOffset - z_def_offset;
+}
+
+bool morobot_p::checkIfAngleValid(uint8_t servoId, float angle){
+	// The values are NAN if the inverse kinematics does not provide a solution
+	if(isnan(angle)){
+		Serial.print("Angle for motor ");
+		Serial.print(servoId);
+		Serial.println(" is NAN!");
+		return false;
+	}
+	
+	// Moving the motors out of the joint limits may harm the robot's mechanics
+	if(angle < _jointLimits[servoId][0] || angle > _jointLimits[servoId][1]) {
+		Serial.print("Angle for motor ");
+		Serial.print(servoId);
+		Serial.print(" is invalid! (");
+		Serial.print(angle);
+		Serial.println(" degrees).");
+		return false;
+	}
+	
+	// Check difference between servo1 and servo2 (in case only one motor moves at a time)	
+	if (servoId == 1 || servoId == 2) {
+		long servo1Angle = angle;
+		long servo2Angle = angle;
+		if (servoId == 1) servo2Angle = getActAngle(2);
+		else if (servoId == 2) servo1Angle = getActAngle(1);
+		return checkIfAngleDiffValid(servo1Angle, servo2Angle);
+	}
+	
+	return true;
+}
+
+bool morobot_p::checkIfAngleDiffValid(float servo1Angle, float servo2Angle){
+	if (90 - servo1Angle - servo2Angle < 20){
+		Serial.println("Difference between motor2 and motor3 too small - could harm mechanics");
+		return false;
+	}
+	
+	if (90 - servo1Angle - servo2Angle > 135){
+		Serial.println("Difference between motor2 and motor3 too big - could harm mechanics");
+		return false;		
+	}
+	
+	return true;
+}
+
+void morobot_p::moveHome(){
+	moveToAngle(0, 0);
+	moveToAngle(1, 75);
+	moveToAngle(2, -25);
+	waitUntilIsReady();
+}
+
+/* PROTECTED FUNCTIONS */
+bool morobot_p::calculateAngles(float x, float y, float z){
+	// Subtract offset
+	x = x - _tcpOffset[0];
+	y = y - _tcpOffset[1];
+	z = z - _tcpOffset[2];
+	
+	// Helper calculations
+	float a1_sq = pow(a1, 2);
+	float a2_sq = pow(a2, 2);
+	float s_sq = pow(sqrt(pow(x, 2) + pow(y, 2)) - a3, 2) + pow(z - d1, 2);
+	float s = sqrt(s_sq);
+	
+	// Calculate angles
+	float theta1 = atan2(y, x);
+	float theta2 = acos((a1_sq + s_sq - a2_sq) / (2 * a1 * s)) + asin((z - d1) / s);
+	float theta3 = acos((a1_sq - s_sq + a2_sq) / (2 * a1 * a2));
+	
+	// Change to degrees
+	theta1 = theta1*180/M_PI;
+	theta3 = -1 * ((theta3 + theta2 - M_PI/2)*180/M_PI);
+	theta2 = theta2*180/M_PI;
+	
+	//Check if angles are valid
+	if(checkIfAngleValid(0, theta1) == false) return false;
+	if(checkIfAngleValid(1, theta2) == false) return false;
+	if(checkIfAngleValid(2, theta3) == false) return false;
+	
+	// Store values
+	_goalAngles[0] = theta1;
+	_goalAngles[1] = theta2;
+	_goalAngles[2] = theta3;
+	
+	return true;
+}
+
+void morobot_p::updateTCPpose(bool output){
+	setBusy();
+	waitUntilIsReady();
+	
+	// Get motor angles
+	float theta1 = getActAngle(0);
+	float theta2 = getActAngle(1);
+	float theta3 = -getActAngle(2);
+	
+	// Recalculate angles and convert to radians
+    theta3 = theta3 - 90 - theta2;
+ 	theta1 = theta1 * M_PI/180;
+	theta2 = theta2 * M_PI/180;
+	theta3 = theta3 * M_PI/180;
+	
+	float x = a1 * cos(theta1) * cos(theta2) + a2 * cos(theta1) * cos(theta2 + theta3) + a3 * cos(theta1);
+	float y = a1 * sin(theta1) * cos(theta2) + a2 * sin(theta1) * cos(theta2 + theta3) + a3 * sin(theta1);
+    float z = d1 + a1 * sin(theta2) + a2 * sin(theta2 + theta3);
+    
+	// Store position
+	_actPos[0] = x + _tcpOffset[0];
+	_actPos[1] = y + _tcpOffset[1];
+	_actPos[2] = z + _tcpOffset[2];
+	
+	// Caculate orientation
+	_actOri[0] = 0;
+	_actOri[1] = 0;
+	_actOri[2] = theta1 * 180/M_PI;
+	
+	if (output == true){
+		printTCPpose();
+	}
+}
